608eb8eef7afc617c1aa158e4c75df1b09c94427
hchiam <hchiam@users.noreply.github.com>
2022-12-16 01:25:08 -0700

    some cleanup of notes

--- example_usage.py
+++ example_usage.py
@@ -1,5 +1,8 @@
-# python3 -m pip install example_package_hchiam
+# pip3 install example_package_hchiam
 from example_package_hchiam import hi
 from example_package_hchiam.innermodule import add_one
 hi()
 print(add_one(1))
+
+# https://test.pypi.org/project/hchiam
+# https://pypi.org/project/hchiam
